5b09e2cb21d063b5507834c1471f06de2408e6ed
Pedro Teles <162949712+eusouopeu@users.noreply.github.com>
2024-05-05 11:54:39 +0000

    importando os codigos que já tinha feito

---
+++ script.js
@@ -0,0 +1,8 @@
+function mostrarMenu() {
+    let hmenu = document.getElementById("hmenu");
+    if (hmenu.style.visibility === "hidden") {
+        hmenu.style.visibility = "visible";
+    } else {
+        hmenu.style.visibility = "hidden";
+    };
+};
